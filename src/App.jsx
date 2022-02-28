29eb09c2915c8110d5210416d503d57c234fae79
Michael Yakovlev <kvadrofilii@gmail.com>
2022-02-28 10:26:57 +0300

    feat: add Routing for App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import GameListPage from './pages/GameListPage';
+import GameProfilePage from './pages/GameProfilePage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={'/'} element={<Layout />} >
+        <Route index element={<GameListPage />} />
+        <Route path={'game/:slug'} element={<GameProfilePage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
