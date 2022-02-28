f5a6e7229c99479e4ffb6b71391fc9e736253e86
Michael Yakovlev <kvadrofilii@gmail.com>
2022-02-28 10:29:22 +0300

    feat: add Input component

---
+++ src/styles/components/Input.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+	display: block;
+	width: 250px;
+	height: 40px;
+	padding: 10px;
+	font-size: 1.6rem;
+	font-style: normal;
+	font-weight: normal;
+	line-height: 150%;
+	border-radius: 10px;
+	border: 1px solid #ccc;
+`;
